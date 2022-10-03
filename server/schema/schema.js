7dc18186b8001a372b7b41e26e5cfc34506b1e00
Barkley <barkley.santo@gmail.com>
2022-10-03 17:42:34 +0700

    added ClientType and ProjectType - able to seein GraphiQL - clients and projects from sample data now visible

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,6 +5,8 @@ const {
   GraphQLID,
   GraphQLString,
   GraphQLSchema,
+  GraphQLList,
+  graphqlSync,
 } = require("graphql")
 
 // Client Type
@@ -18,10 +20,40 @@ const ClientType = new GraphQLObjectType({
   }),
 })
 
+// Project type
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+  }),
+})
+
 // Root query object
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } }, // we pass in id we want for this client
+      resolve(parent, args) {
+        return projects.find((project) => project.id === args.id)
+      },
+    },
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return clients
+      },
+    },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } }, // we pass in id we want for this client
