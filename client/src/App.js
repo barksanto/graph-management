842fe3b7a43ff53b11535ab8c54bca488f9d3340
Barkley <barkley.santo@gmail.com>
2022-10-06 15:08:39 +0700

    added bootstrap style library

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div className="container">
       <h1>Hello World!</h1>
       <img
-        src="./components/assets/graphlogo.png"
+        src="https://images.unsplash.com/photo-1664790008528-e8cde9588180?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
         alt="logo"
         style={{ marginRight: "10em" }}
       />
