5eefc1c8c14fa4dcc54e114657701d6cd4773f41
Barkley <barkley.santo@gmail.com>
2022-10-06 15:04:34 +0700

    create react app - added dependencies & styling

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello World!</h1>
+      <img
+        src="./components/assets/graphlogo.png"
+        alt="logo"
+        style={{ marginRight: "10em" }}
+      />
+    </div>
+  )
+}
+
+export default App
