09daaa68241d549874ab4ea999eaa22037c615fe
Barkley <barkley.santo@gmail.com>
2022-10-11 11:54:01 +0700

    refetch Clients list after mutation using refetchQueries for the deleteClient function- it updates the UI

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,13 +1,21 @@
-import {FaTrash} from 'react-icons/fa'
+import { FaTrash } from 'react-icons/fa'
+import { useMutation } from '@apollo/client'
+import { DELETE_CLIENT } from '../mutations/clientMutations'
+import { GET_CLIENTS } from './queries/clientQueries' // <-- use this to refetch clients after mutation is complete
 
 export default function ClientRow({ client }) {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    refetchQueries: [{query: GET_CLIENTS}] // <-- this is the query we want to refetch after the mutation
+  })
+
    return (
       <tr key={client.id}>
         <td>{client.name}</td>
         <td>{client.email}</td>
         <td>{client.phone}</td>
         <td>
-          <button className='btn btn-danger btn-sm'><FaTrash/></button>
+          <button className='btn btn-danger btn-sm' onClick={deleteClient}><FaTrash/></button>
         </td>
       </tr>
       )
