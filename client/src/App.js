2badabd3b9344c3d4561ca97f559088d47b1447c
Barkley <barkley.santo@gmail.com>
2022-10-13 13:20:56 +0700

    created project card - iterate through projects and dynamically show a card for each - create Projects.jsx which will render one instance of ProjectCard for each project - GET_PROJECTS and useQuery gets the data - destcucture in function' '

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
 import Header from "./components/Header"
 import Clients from "./components/Clients"
+import Projects from "./components/Projects"
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
 import AddClientModal from "./components/AddClientModal"
 
 const cache = new InMemoryCache({
+  // this is the cache
   typePolicies: {
     Query: {
       fields: {
         clients: {
           merge(existing, incoming) {
-            return incoming
+            return incoming // this returns the incoming data for clients
           },
         },
         projects: {
           merge(existing, incoming) {
-            return incoming
+            return incoming // this returns incoming data for projects
           },
         },
       },
@@ -34,6 +36,7 @@ function App() {
         <Header />
         <div className="container">
           <AddClientModal />
+          <Projects/>
           <Clients />
         </div>
       </ApolloProvider>
