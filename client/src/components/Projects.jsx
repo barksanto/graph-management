95baadee0dd5153a904ce6fc2254fc081352c83d
Barkley <barkley.santo@gmail.com>
2022-10-14 14:20:08 +0700

    create Project, Home and NotFound components - implementing ReactRouter to direct to appropriate project page when clicking View for the project

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -11,7 +11,7 @@ function Projects() {
 
   return <>
     {data.projects.length > 0 ? (
-      <div className="row">
+      <div className="row mt-4">
         {data.projects.map((project) => (
           <ProjectCard className="col-md-4" key={project.id} project={ project} />
           ))}
