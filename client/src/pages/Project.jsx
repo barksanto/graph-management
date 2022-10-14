20cef678958e84357540ff29adaf1e1eb762260a
Barkley <barkley.santo@gmail.com>
2022-10-14 14:49:00 +0700

    git add .

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,8 +1,17 @@
-
+import { Link, useParams } from 'react-router-dom'
+import Spinner from '../components/Spinner';
+import { useQuery } from '@apollo/client'
+import { GET_PROJECT } from '../components/queries/projectQueries'
 
 function Project() {
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_PROJECT, { variables: { id } });
+
+  if (loading) return <Spinner />;
+  if (error) return <p>Something Went Wrong</p>;
+
   return (
-    <div>Project</div>
+    <div>banans</div>
   )
 }
 
